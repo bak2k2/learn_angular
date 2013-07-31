a3553a28bf803e4fb2d9166c830ea62cad1ac937
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-07-30 17:56:43 -0700

    Removing out and target folders

--- out/artifacts/metrics_war_exploded/js/services/project_services.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('projectServices', ['ngResource']).
-    factory('Project', function($resource){
-        return $resource('resources/projects', {}, {
-            query: {method:'GET', isArray:true}
-        });
-    });
