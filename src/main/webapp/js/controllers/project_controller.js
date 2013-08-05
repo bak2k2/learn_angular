371533544ff6921daf15cb93c36fb1d0b5b6a134
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-05 13:58:45 -0700

    added javascript cloneData for cloning data objects

--- src/main/webapp/js/controllers/project_controller.js
+++ src/main/webapp/js/controllers/project_controller.js
@@ -1,19 +1,28 @@
 'use strict';
 
+function cloneData(obj)
+{
+    return JSON.parse(JSON.stringify(obj));
+}
+
 function ProjectListCtrl($scope, Project, Projects) {
+
+    $scope.editMode = false;
 
     $scope.projects = Projects.query();
 
-    $scope.greet = function(id){
+    $scope.select = function(id){
         var resp = Project.get({id: id}, function(project){
             $scope.project = project;
+            $scope.editable_project = cloneData($scope.project);
         });
     }
 
     $scope.edit = function(){
         $scope.editMode = !$scope.editMode;
+        if ($scope.editMode)
+            $scope.editable_project = cloneData($scope.project);
     }
 
-    $scope.editMode = false;
 }
 
