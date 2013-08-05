b709a24489ee8a50b4704451bad68bdd2c345008
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-05 16:32:21 -0700

    fixing the model binding for view/edit and clone the backend object

--- src/main/webapp/js/controllers/project_controller.js
+++ src/main/webapp/js/controllers/project_controller.js
@@ -6,23 +6,17 @@ function cloneData(obj)
 }
 
 function ProjectListCtrl($scope, Project, Projects) {
-
     $scope.editMode = false;
-
     $scope.projects = Projects.query();
-
     $scope.select = function(id){
         var resp = Project.get({id: id}, function(project){
             $scope.project = project;
             $scope.editable_project = cloneData($scope.project);
         });
     }
-
     $scope.edit = function(){
         $scope.editMode = !$scope.editMode;
-        if ($scope.editMode)
-            $scope.editable_project = cloneData($scope.project);
+        $scope.editable_project = cloneData($scope.project);
     }
-
 }
 
