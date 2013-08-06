9766a496599df2a1d6c1db56df50b0375dc8675a
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-06 11:28:57 -0700

    moved all the partials to project subfolder

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -5,6 +5,6 @@ var metricsModule = angular.module('metrics', ['ngResource']);
 metricsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/admin', {templateUrl: 'partials/admin_list.html',   controller: ProjectHomeCtrl}).
-        when('/projects', {templateUrl: 'partials/project_home.html',   controller: ProjectHomeCtrl}).
+        when('/projects', {templateUrl: 'partials/project/project_home.html',   controller: ProjectHomeCtrl}).
         otherwise({redirectTo: '/projects'});
 }]);
