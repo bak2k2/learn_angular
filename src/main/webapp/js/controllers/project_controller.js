78d7320b93d126e2b3ccd6c3cb77eeef74b429b8
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-05 21:31:19 -0700

    more cleanup

--- src/main/webapp/js/controllers/project_controller.js
+++ src/main/webapp/js/controllers/project_controller.js
@@ -28,16 +28,15 @@ function ProjectHomeCtrl($scope, ProjectGateway, ProjectsGateway) {
         ProjectGateway.save({id: $scope.editable_project.id}, $scope.editable_project, onSave);
     }
 
+    function onSave(project){
+        $scope.project = project;
+        setEditMode(false);
+        $scope.$broadcast('metricsApp.projectSaved');
+    }
+
     function setEditMode(isEditMode){
         $scope.editMode = isEditMode;
         $scope.editable_project = cloneData($scope.project);
-    }
-
-    function onSave(project){
-        $scope.editMode = false;
-        $scope.project = project;
-        $scope.editable_project = cloneData(project);
-        $scope.$broadcast('metricsApp.projectSaved');
     }
 
     $scope.$on('metricsApp.projectSaved', function(){
