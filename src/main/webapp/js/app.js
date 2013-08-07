f9d4f26c75fdbda9a39e9accecce1a5b318dc1b3
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-06 17:53:27 -0700

    renaming files

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,8 +4,8 @@ var metricsModule = angular.module('metrics', ['ngResource']);
 
 metricsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/admin', {templateUrl: 'partials/admin/admin_home.html',   controller: AdminHomeCtrl}).
+        when('/admin', {templateUrl: 'partials/admin/home.html',   controller: AdminHomeCtrl}).
         when('/admin/iterations', {templateUrl: 'partials/admin/iterations.html',   controller: AdminHomeCtrl}).
-        when('/projects', {templateUrl: 'partials/project/project_home.html',   controller: ProjectHomeCtrl}).
+        when('/projects', {templateUrl: 'partials/project/home.html',   controller: ProjectHomeCtrl}).
         otherwise({redirectTo: '/projects'});
 }]);
