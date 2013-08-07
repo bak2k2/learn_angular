8817e148208dbe66339cfb76bea5ff25cc06a1a0
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-06 18:27:18 -0700

    adding projects template for admin tab

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -6,6 +6,7 @@ metricsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/admin', {templateUrl: 'partials/admin/home.html',   controller: AdminHomeCtrl}).
         when('/admin/iterations', {templateUrl: 'partials/admin/iterations.html',   controller: AdminHomeCtrl}).
+        when('/admin/projects', {templateUrl: 'partials/admin/projects.html',   controller: AdminHomeCtrl}).
         when('/projects', {templateUrl: 'partials/project/home.html',   controller: ProjectHomeCtrl}).
         otherwise({redirectTo: '/projects'});
 }]);
