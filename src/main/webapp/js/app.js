5156df3fbd29a40e7a5a8dc9eee6f65202cea52d
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-08 14:54:24 -0700

    added dashboard home page and controller

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -5,8 +5,9 @@ var metricsModule = angular.module('metrics', ['ngResource']);
 metricsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/admin', {templateUrl: 'partials/admin/home.html',   controller: AdminHomeCtrl}).
+        when('/dashboard', {templateUrl: 'partials/dashboard/home.html', controller: DashboardCtrl}).
         when('/admin/iterations', {templateUrl: 'partials/admin/iterations.html',   controller: AdminHomeCtrl}).
         when('/admin/projects', {templateUrl: 'partials/admin/projects.html',   controller: AdminHomeCtrl}).
         when('/projects', {templateUrl: 'partials/project/home.html',   controller: ProjectHomeCtrl}).
-        otherwise({redirectTo: '/projects'});
+        otherwise({redirectTo: '/dashboard'});
 }]);
