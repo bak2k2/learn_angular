6b3c82d62a518f978e61bcfaf7040e384530bb08
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-09 01:29:12 -0700

    adding missed file

---
+++ src/main/java/com/gap/metrics/dto/ProjectMetric.java
@@ -0,0 +1,15 @@
+package com.gap.metrics.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Document
+public class ProjectMetric {
+    private List<Double> averageVelocities;
+    private List<String> iterationNames;
+}
