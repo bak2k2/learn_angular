acfd76f85e3b0a4c93fa5c8f16567416e80e149c
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-08-09 13:01:56 -0700

    cleaning up dashboard charts

--- src/main/webapp/js/dashboard/cycletime_trend.js
+++ src/main/webapp/js/dashboard/cycletime_trend.js
@@ -34,23 +34,22 @@ app.directive('cyclehighchart', function () {
         replace: true,
 
         link: function (scope, element, attrs) {
-            scope.$watch(function() { return attrs.chartc; }, function() {
-                if(!attrs.chartc) return;
-                var chartc = scope.cycletime_trend_chart;
-                element.highcharts(chartc);
+            scope.$watch(function() { return attrs.cyclechart; }, function() {
+                if(!attrs.cyclechart) return;
+                var cyclechart = scope.cycletime_trend_chart;
+                element.highcharts(cyclechart);
             });
         }
     }
 });
 
 function CycleTimeTrendCtrl($scope, $http) {
-    chartc = get_cycletime_chart();
-    //url = '/resources/project/90699d25-23a0-4908-b21d-d20182768d84/velocities';
+    cyclechart = get_cycletime_chart();
     url = '/resources/project/averagecycletimes';
     $http({method: 'GET', url: url}).
         success(function(data, status, headers, config) {
-            chartc.series[0].data = data.averageCycleTimes;
-            chartc.xAxis.categories = data.iterationNames;
-            $scope.cycletime_trend_chart = chartc;
+            cyclechart.series[0].data = data.averageCycleTimes;
+            cyclechart.xAxis.categories = data.iterationNames;
+            $scope.cycletime_trend_chart = cyclechart;
         });
 }
--- src/main/webapp/js/dashboard/velocity_trend.js
+++ src/main/webapp/js/dashboard/velocity_trend.js
@@ -1,4 +1,4 @@
-function get_chart() {
+function get_velocity_chart() {
     return {
         title: {
             text: 'Velocity Trend',
@@ -27,36 +27,29 @@ function get_chart() {
     };
 }
 
-app.directive('highchart', function () {
-    console.log("returning directive for velocity app");
+app.directive('velocityhighchart', function () {
     return {
         restrict: 'E',
         template: '<div></div>',
         replace: true,
 
         link: function (scope, element, attrs) {
-            console.log("inside link function of velocity directive")
-            scope.$watch(function() { return attrs.chart; }, function() {
-                if(!attrs.chart) return;
-                var chart = scope.velocity_trend_chart;
-                element.highcharts(chart);
-                console.log("set velocity highcharts")
+            scope.$watch(function() { return attrs.velocitychart; }, function() {
+                if(!attrs.velocitychart) return;
+                var velocitychart = scope.velocity_trend_chart;
+                element.highcharts(velocitychart);
             });
         }
     }
 });
 
 function VelocityTrendCtrl($scope, $http) {
-    console.log("inside velocity controller");
-    chart = get_chart();
-    //url = '/resources/project/90699d25-23a0-4908-b21d-d20182768d84/velocities';
+    velocitychart = get_velocity_chart();
     url = '/resources/project/averagevelocities';
     $http({method: 'GET', url: url}).
         success(function(data, status, headers, config) {
-            //chart.series[0].data = [10,23,14,11,5,21];
-            chart.series[0].data = data.averageVelocities;
-            chart.xAxis.categories = data.iterationNames;
-            $scope.velocity_trend_chart = chart;
-
+            velocitychart.series[0].data = data.averageVelocities;
+            velocitychart.xAxis.categories = data.iterationNames;
+            $scope.velocity_trend_chart = velocitychart;
         });
 }
