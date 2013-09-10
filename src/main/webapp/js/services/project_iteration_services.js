41419385ea2eb98ebeaa28bbae2243195728c27d
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2013-09-10 16:45:14 -0700

    removing unwanted services

--- src/main/webapp/js/services/project_iteration_services.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-metricsModule.factory('ProjectIterationGateway', function($resource){
-    return $resource('resources/project/:projectId/iteration/:iterationId', {}, {
-        get: {method:'GET', isArray:false},
-        save: {method:'POST', params:{projectId: '@projectId', iterationId: '@iterationId'}},
-    });
-});
